2986b5a6e4734c02a6094146ec239f8d3797aa93
Marichelle Bisuna <marichellebisuna@yahoo.com>
2023-02-08 11:17:09 +1100

    Hotdeals UI done

---
+++ frontend/src/components/hotdeals/Hotdeals.js
@@ -0,0 +1,83 @@
+import './hotdeals.scss'
+import blue from '../../assets/product/blue.png'
+const Hotdeals = () => {
+  return (
+    <div className='hotdeals'>
+      <div className="left">
+       <div className="image-here">
+        <div className="icons">
+          <div className="icon"><i class="fa-solid fa-chart-simple"></i></div>
+          <div className="icon"> <i class="fa-solid fa-magnifying-glass-plus"></i></div>
+          <div className="icon"><i class="fa-regular fa-heart"></i></div>
+          <div className="icon"><i class="fa-solid fa-bag-shopping"></i></div>
+        </div>
+        <div className="image">
+          <img src={blue} alt="" />
+        </div>
+        <div className="details">
+          <div className="title">armani exchange</div>
+          <div className="price">$30.00 <span className='strike'>$45.00</span></div>
+          <div className="rating">
+          <i class="fa-solid fa-star"></i>
+          <i class="fa-solid fa-star"></i>
+          <i class="fa-solid fa-star"></i>
+          <i class="fa-solid fa-star"></i>
+          <i class="fa-regular fa-star"></i>
+          </div>
+        </div>
+       </div>
+       <div className="count-down">
+        <div className="timer">
+        <div className="time"><span className='blue'>02</span>days</div>
+          <div className="time"><span className='blue'>21</span>hrs</div>
+          <div className="time"><span className='blue'>44</span>mins</div>
+          <div className="time"><span className='blue'>50</span>secs</div>
+        </div>
+        <div className="line"></div>
+        <div className="title"><h1 ><span style={{color:"red", fontWeight:"700"}}> Hot deal </span>this week</h1></div>
+        <div className="desc"><h3 style={{fontWeight:"400"}}>tables & chairs up to 30% off</h3></div>
+        <button type="submit">shop now</button>
+       </div>
+      </div>
+
+      <div className="right">
+       <div className="image-here">
+       <div className="icons">
+          <div className="icon"><i class="fa-solid fa-chart-simple"></i></div>
+          <div className="icon"> <i class="fa-solid fa-magnifying-glass-plus"></i></div>
+          <div className="icon"><i class="fa-regular fa-heart"></i></div>
+          <div className="icon"><i class="fa-solid fa-bag-shopping"></i></div>
+        </div>
+        <div className="image"> <img src={blue} alt="" /></div>
+        <div className="details">
+        <div className="title">armani exchange</div>
+          <div className="price">$30.00 <span className='strike'>$45.00</span></div>
+          <div className="rating">
+          <i class="fa-solid fa-star"></i>
+          <i class="fa-solid fa-star"></i>
+          <i class="fa-solid fa-star"></i>
+          <i class="fa-solid fa-star"></i>
+          <i class="fa-solid fa-star"></i>
+          </div>
+        </div>
+       </div>
+       <div className="count-down">
+        <div className="timer">
+          <div className="time"><span className='blue'>02</span>days</div>
+          <div className="time"><span className='blue'>21</span>hrs</div>
+          <div className="time"><span className='blue'>44</span>mins</div>
+          <div className="time"><span className='blue'>50</span>secs</div>
+        </div>
+        <div className="line"></div>
+
+        <div className="title"><h1 ><span style={{color:"red", fontWeight:"700"}}> Hot deal </span>this week</h1></div>
+        <div className="desc"><h3 style={{fontWeight:"400"}}>tables & chairs up to 30% off</h3></div>
+        <button type="submit">shop now</button>
+       </div>
+      </div>
+
+    </div>
+  )
+}
+
+export default Hotdeals
--- frontend/src/pages/home/Home.js
+++ frontend/src/pages/home/Home.js
@@ -1,5 +1,6 @@
-import Featured from '../../components/featured/Featured'
+import Hotdeals from '../../components/hotdeals/Hotdeals'
 import './home.scss'
+// import Featured from '../../components/featured/Featured'
 // import Instagram from '../../components/instagram/Instagram'
 // import Banner from '../../components/banner/Banner'
 
@@ -7,8 +8,9 @@ const Home = () => {
   
   return (   
     <div>
-     <div className='home'>Home</div> 
-      <Featured />
+     <div className='home'>Home</div>
+     <Hotdeals/> 
+      {/* <Featured /> */}
      {/* <Banner />
      <Instagram /> */}
     </div> 
