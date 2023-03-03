bd79b8c3d622fc36e09bb5b0b01477179cd05909
Marichelle Bisuna <marichellebisuna@yahoo.com>
2023-03-03 12:11:47 +1100

    Forgotpassword UI design done

--- frontend/src/pages/forgotpassword/ForgotPassword.js
+++ frontend/src/pages/forgotpassword/ForgotPassword.js
@@ -1,23 +1,16 @@
-import './login.scss'
+import './forgotpassword.scss'
 import { Link} from 'react-router-dom'
 
 const ForgotPassword = () => {
   return (
     <div className="forgotpassword">
       <h1>Forgot Password</h1>
+      <div className="text">No worries. We'll send you a reset instructions.</div>
       <div className="container">
-        <label>Username or email address*</label>
+        <label>Email address*</label>
         <input type="email" name="" id="" />
-        <label>Password*</label>
-        <input type="password" name="" id="" />
-        <div className="remember">
-          <div className="me">
-            <input type="checkbox" name="" id="" />
-            <label>Remember me</label>
-          </div>
-          <div className="text"><Link to="/forgotpassword"></Link>Forgot your password?</div>
-        </div>
-       <button type="submit">Login</button>
+        
+       <button type="submit">Reset password</button>
       </div>
     </div>
   )
